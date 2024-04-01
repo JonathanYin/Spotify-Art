cc5486b873d11b8ab9e62e7916fb8f63a6ff90ed
JonathanYin <joyin@ucsd.edu>
2024-04-01 15:24:46 -0700

    Output playlist songs

--- src/spotify_client.py
+++ src/spotify_client.py
@@ -14,18 +14,13 @@ class SpotifyClient:
             redirect_uri=os.getenv('SPOTIFY_REDIRECT_URI'),
             scope="playlist-read-private user-read-email"))
 
-
-    # Add methods to interact with the Spotify API here
     def get_current_user_profile(self):
         return self.sp.current_user()
-    
+
     def get_playlist_tracks_with_cover_art(self, playlist_id):
         tracks_with_cover_art = []
         results = self.sp.playlist_items(playlist_id)
-        tracks = results['items']
-        while results['next']:
-            results = self.sp.next(results)
-            tracks.extend(results['items'])
+        tracks = self._get_all_items(results)
         
         for track in tracks:
             track_info = track['track']
@@ -39,8 +34,17 @@ class SpotifyClient:
         
         return tracks_with_cover_art
 
+    def _get_all_items(self, initial_results):
+        """
+        Handles pagination for Spotify API calls that return a collection of items.
+        """
+        items = initial_results['items']
+        while initial_results['next']:
+            initial_results = self.sp.next(initial_results)
+            items.extend(initial_results['items'])
+        return items
+
 if __name__ == '__main__':
-    # Initialize the SpotifyClient
     spotify_client = SpotifyClient()
     
     # Fetch the current user's profile
@@ -52,3 +56,9 @@ if __name__ == '__main__':
     print(f"Email: {user_profile.get('email')}")
     print(f"Country: {user_profile.get('country')}")
 
+    playlist_id = '3seDFtPQlYCdw0ymhWOPoq'
+    tracks_with_cover_art = spotify_client.get_playlist_tracks_with_cover_art(playlist_id)
+    
+    # Print the results
+    for track in tracks_with_cover_art:
+        print(f"Track ID: {track['id']}, Name: {track['name']}, Cover Art URL: {track['cover_art_url']}")
