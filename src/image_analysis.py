4386b22602d1dd40d56bdc5cf88bdf9debe16c27
JonathanYin <joyin@ucsd.edu>
2024-04-10 15:15:51 -0700

    Update with OpenCV color visualization, initialize environment.yml file

--- src/image_analysis.py
+++ src/image_analysis.py
@@ -1,57 +1,76 @@
 import cv2
 import numpy as np
-import requests
 import boto3
+from collections import Counter
+import matplotlib.pyplot as plt
 
-# Create an S3 resource
-s3 = boto3.resource('s3')
+# Initialize a boto3 client for AWS S3 access
+s3_client = boto3.client('s3')
 
-def download_image(url):
-    response = requests.get(url)
-    image = np.array(bytearray(response.content), dtype=np.uint8)
-    image = cv2.imdecode(image, -1)  # '-1' to read the image as is, including the alpha channel
+def download_image_from_s3(bucket_name, object_key):
+    """
+    Downloads an image from an AWS S3 bucket and converts it into a format suitable for OpenCV processing.
+    
+    Parameters:
+    - bucket_name (str): The name of the S3 bucket.
+    - object_key (str): The S3 object key where the image is stored.
+    
+    Returns:
+    - image (numpy.ndarray): The downloaded image as a NumPy array in OpenCV format.
+    """
+    s3_response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+    image_content = s3_response['Body'].read()
+    image_as_np_array = np.frombuffer(image_content, dtype=np.uint8)
+    image = cv2.imdecode(image_as_np_array, flags=cv2.IMREAD_COLOR)
     return image
 
-def display_image(image):
-    """Display the image using OpenCV"""
-    cv2.imshow('Image', image)
-    cv2.waitKey(0)  # Wait for a key press to close
-    cv2.destroyAllWindows()
-
-def upload_file_to_s3(file_name, bucket_name, object_name=None):
+def find_dominant_colors(image, k=3):
     """
-    Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket_name: Bucket to upload to
-    :param object_name: S3 object name. If not specified, file_name is used
-    :return: True if file was uploaded, else False
+    Identifies the k dominant colors in an image using k-means clustering.
+    
+    Parameters:
+    - image (numpy.ndarray): The image for which to find the dominant colors, in OpenCV format.
+    - k (int): The number of dominant colors to identify.
+    
+    Returns:
+    - dominant_colors_bgr (list of lists): The dominant colors found in the image, in BGR format.
     """
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = file_name
+    pixels = image.reshape((-1, 3))
+    pixels = np.float32(pixels)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+    _, labels, centroids = cv2.kmeans(pixels, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    label_counts = Counter(labels.flatten())
+    common_labels = label_counts.most_common(k)
+    dominant_colors = [centroids[label].tolist() for label, _ in common_labels]
+    dominant_colors_bgr = [color[::-1] for color in dominant_colors]
+    return dominant_colors_bgr
 
-    # Upload the file
-    try:
-        s3.meta.client.upload_file(file_name, bucket_name, object_name)
-    except Exception as e:
-        print(f"Upload failed: {e}")
-        return False
-    return True
+def visualize_colors(dominant_colors_bgr):
+    """
+    Visualizes the dominant colors found in an image.
+    
+    Parameters:
+    - dominant_colors_bgr (list of lists): The dominant colors to visualize, in BGR format.
+    """
+    dominant_colors_rgb = [color[::-1] for color in dominant_colors_bgr]
+    for color in dominant_colors_rgb:
+        color_square = np.array([[[component / 255 for component in color] for _ in range(10)] for _ in range(10)])
+        plt.figure(figsize=(2,2))
+        plt.axis("off")
+        plt.imshow(color_square)
+        plt.show()
 
 if __name__ == '__main__':
-    # Image URL
-    file_name = '../images/test.jpeg'
     bucket_name = 'spotify-playlist-images'
-    # Optionally, specify a different object name in S3
-    object_name = 'test.jpg'  # You can change this to organize files in folders, etc.
+    object_key = '3seDFtPQlYCdw0ymhWOPoq/cover_art/0CPWIY1etGeJLg12CAuP4n.jpg'
 
-    upload_successful = upload_file_to_s3(file_name, bucket_name, object_name)
-    if upload_successful:
-        print("Upload successful.")
-    else:
-        print("Upload failed.")
+    # Download the image from S3
+    image = download_image_from_s3(bucket_name, object_key)
+
+    # Find dominant colors
+    dominant_colors = find_dominant_colors(image, k=3)
     
-    # test_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/OpenCV_Logo_with_text_svg_version.svg/800px-OpenCV_Logo_with_text_svg_version.svg.png'
-    # image = download_image(test_url)
-    # display_image(image)
+    print("Dominant colors (in BGR):", dominant_colors)
+
+    # Visualize the dominant colors
+    visualize_colors(dominant_colors)
